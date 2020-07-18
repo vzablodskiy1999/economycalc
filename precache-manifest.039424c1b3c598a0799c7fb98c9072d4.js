1058b9b30331138141026f275f22c46aef1c47b3
Valeriy Zablodskiy <vzablodskiy1999@gmail.com>
2020-07-18 13:14:29 +0300

    Updates

---
+++ precache-manifest.039424c1b3c598a0799c7fb98c9072d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98555916d85f70749ac74293f4e95795",
+    "url": "/economycalc/index.html"
+  },
+  {
+    "revision": "22f9dcfd3ae813cee2a7",
+    "url": "/economycalc/static/css/2.e9c4461c.chunk.css"
+  },
+  {
+    "revision": "2d621ec542a8d1ea793d",
+    "url": "/economycalc/static/css/main.2218e3f6.chunk.css"
+  },
+  {
+    "revision": "22f9dcfd3ae813cee2a7",
+    "url": "/economycalc/static/js/2.2db8ac7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/economycalc/static/js/2.2db8ac7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d621ec542a8d1ea793d",
+    "url": "/economycalc/static/js/main.4ad75bd3.chunk.js"
+  },
+  {
+    "revision": "9a4eccc93c6ddf7176f5",
+    "url": "/economycalc/static/js/runtime-main.a4f6ad64.js"
+  }
+]);
